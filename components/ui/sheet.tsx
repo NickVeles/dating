e569968c64f3e477762fd749d8fb860f73fa30d5
NickVeles <xm1k123x@gmail.com>
2025-05-27 23:10:41 +0200

    fix: increase close button icon size in SheetContent for better visibility

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -73,7 +73,7 @@ function SheetContent({
       >
         {children}
         <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none hover:cursor-pointer">
-          <X className="w-[1.2rem] h-[1.2rem]" />
+          <X className="w-[1.4rem] h-[1.4rem]" />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
