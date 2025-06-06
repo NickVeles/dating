9cff1d5f212fb96c14f00d46bfdfe752abfde0f8
NickVeles <xm1k123x@gmail.com>
2025-06-06 23:46:14 +0200

    fix: add dyslexic font support in TitleContainer for improved accessibility

--- components/utilities/title-container.tsx
+++ components/utilities/title-container.tsx
@@ -10,7 +10,9 @@ export default function TitleContainer({
   return (
     <div className="flex flex-col gap-2 mt-[5.5rem]">
       {Icon && <Icon className="w-24 h-24 mx-auto text-primary" />}
-      <h1 className="text-4xl text-center font-bold font-serif">{children}</h1>
+      <h1 className="text-4xl text-center font-bold font-serif dyslexic:font-[family-name:var(--font-dyslexic)]">
+        {children}
+      </h1>
     </div>
   );
 }
