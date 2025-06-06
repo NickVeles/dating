246a9547b36ecd058904ac52b55428056f6d81b0
NickVeles <xm1k123x@gmail.com>
2025-06-06 18:48:23 +0200

    feat: add TextLookup component with tooltip functionality

---
+++ components/utilities/text-lookup.tsx
@@ -0,0 +1,27 @@
+import { MagnifyingGlass } from "phosphor-react";
+import { ReactNode } from "react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+
+interface TextLookupProps {
+  text: string;
+  children: ReactNode;
+  className?: string;
+}
+
+export function TextLookup({
+  text,
+  children,
+  className = "hover:underline",
+}: TextLookupProps) {
+  return (
+    <Tooltip>
+      <TooltipContent>{children}</TooltipContent>
+      <TooltipTrigger className={`inline ${className}`}>
+        {text}
+        <MagnifyingGlass className="inline align-text-top size-4" />
+      </TooltipTrigger>
+    </Tooltip>
+  );
+}
+
+export default TextLookup;
