e9dc6dafa783a071d65d70c2a605617ac75f8013
NickVeles <xm1k123x@gmail.com>
2025-06-06 23:23:31 +0200

    feat: add SectionContainer component for flexible layout with optional accent

---
+++ components/utilities/section-container.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type SectionContainerProps = {
+  accented?: boolean;
+  children: React.ReactNode;
+};
+
+export default function SectionContainer({
+  accented,
+  children,
+}: SectionContainerProps) {
+  const innerClass =
+    "sm:max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto w-full p-[1rem] flex flex-col items-center justify-center";
+
+  if (accented) {
+    return (
+      <div className="w-screen bg-accent relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw]">
+        <div className={innerClass}>{children}</div>
+      </div>
+    );
+  }
+  return <div className={innerClass}>{children}</div>;
+}
