9a21d71de780d109ea574e73697c2951c6449128
NickVeles <xm1k123x@gmail.com>
2025-06-06 23:51:08 +0200

    feat: improve title container width and layout

--- components/utilities/title-container.tsx
+++ components/utilities/title-container.tsx
@@ -8,7 +8,7 @@ export default function TitleContainer({
   children,
 }: TitleContainerProps) {
   return (
-    <div className="flex flex-col gap-2 mt-[5.5rem]">
+    <div className="flex flex-col gap-2 mt-[4.5rem] sm:max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto w-full p-[1rem] items-center justify-center">
       {Icon && <Icon className="w-24 h-24 mx-auto text-primary" />}
       <h1 className="text-4xl text-center font-bold font-serif dyslexic:font-[family-name:var(--font-dyslexic)]">
         {children}
