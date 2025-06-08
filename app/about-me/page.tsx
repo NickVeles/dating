6b4e2b17961e8f42137bc6a33f3d10769bdf3c75
NickVeles <xm1k123x@gmail.com>
2025-06-09 01:45:55 +0200

    feat: integrate carousel component with autoplay functionality in About Me section

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import SectionContainer from "@/components/utilities/section-container";
 import TitleContainer from "@/components/utilities/title-container";
 import { useTheme } from "next-themes";
+import Image from "next/image";
 import { UserCircle } from "phosphor-react";
+import Autoplay from "embla-carousel-autoplay";
+
+const images: string[] = [];
 
 export default function AboutMe() {
   const { theme } = useTheme();
@@ -14,6 +25,34 @@ export default function AboutMe() {
   return (
     <main className="flex flex-col flex-1 gap-8">
       <TitleContainer Icon={UserCircle}>About Me</TitleContainer>
+      <SectionContainer>
+        <Carousel
+          opts={{
+            loop: true,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+        >
+          <CarouselContent>
+            {images.map((src, index) => (
+              <CarouselItem key={index}>
+                <Image
+                  src={src}
+                  alt={`Image ${index + 1}`}
+                  width={500}
+                  height={500}
+                  className="object-cover rounded-lg"
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </SectionContainer>
       <SectionContainer accented>
         <p className="text-lg text-justify">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore quo
