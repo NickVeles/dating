b562021eaac3158aeab4f4575654b254e52858c2
NickVeles <xm1k123x@gmail.com>
2025-06-09 04:02:30 +0200

    feat: replace carousel with gallery component in About Me section

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,24 +1,14 @@
 "use client";
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import Gallery from "@/components/gallery";
 import SectionContainer from "@/components/utilities/section-container";
 import TitleContainer from "@/components/utilities/title-container";
 import { useTheme } from "next-themes";
-import Image from "next/image";
 import { UserCircle } from "phosphor-react";
-import Autoplay from "embla-carousel-autoplay";
-
-const images: string[] = [];
 
 export default function AboutMe() {
   const { theme } = useTheme();
-
+    
   // Ensure theme is loaded before rendering
   if (typeof theme === "undefined") return null;
 
@@ -26,32 +16,7 @@ export default function AboutMe() {
     <main className="flex flex-col flex-1 gap-8">
       <TitleContainer Icon={UserCircle}>About Me</TitleContainer>
       <SectionContainer>
-        <Carousel
-          opts={{
-            loop: true,
-          }}
-          plugins={[
-            Autoplay({
-              delay: 2000,
-            }),
-          ]}
-        >
-          <CarouselContent>
-            {images.map((src, index) => (
-              <CarouselItem key={index}>
-                <Image
-                  src={src}
-                  alt={`Image ${index + 1}`}
-                  width={500}
-                  height={500}
-                  className="object-cover rounded-lg"
-                />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+        <Gallery />
       </SectionContainer>
       <SectionContainer accented>
         <p className="text-lg text-justify">
