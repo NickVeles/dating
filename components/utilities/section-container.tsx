4e98b7b411548f4d936304e1ebea9749dcd64ef9
NickVeles <xm1k123x@gmail.com>
2025-06-09 23:17:34 +0200

    refactor: fix section container having wrong flex behavior when accented or not

--- components/utilities/section-container.tsx
+++ components/utilities/section-container.tsx
@@ -7,15 +7,11 @@ export default function SectionContainer({
   accented,
   children,
 }: SectionContainerProps) {
-  const innerClass =
-    "sm:max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto w-full px-[1rem] py-[2rem] flex flex-col items-center justify-center";
-
-  if (accented) {
-    return (
-      <div className="w-screen bg-accent relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw]">
-        <div className={innerClass}>{children}</div>
+  return (
+    <div className={`w-screen relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw] ${accented ? 'bg-accent' : ''}`}>
+      <div className="sm:max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto w-full px-[1rem] py-[2rem] flex flex-col items-center justify-center">
+        {children}
       </div>
-    );
-  }
-  return <div className={innerClass}>{children}</div>;
+    </div>
+  );
 }
