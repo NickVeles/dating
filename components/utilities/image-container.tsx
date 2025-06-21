6123e09ce81a06db03abfd4eeb9e9ea08c6c8c2b
NickVeles <xm1k123x@gmail.com>
2025-06-21 18:35:14 +0200

    feat: enhance ImageContainer with dialog and zoom functionality

--- components/utilities/image-container.tsx
+++ components/utilities/image-container.tsx
@@ -1,32 +1,55 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 type ImageContainerProps = {
   src: string;
   alt?: string;
   className?: string;
-  onClick?: () => void;
 };
 
 export default function ImageContainer({
   src,
   alt,
   className,
-  onClick,
 }: ImageContainerProps) {
   return (
-    <div className={cn("w-full flex justify-center items-center mx-2 my-6", className)}>
-      <Image
-        src={src}
-        alt={alt || "Image"}
-        width={1000}
-        height={1000}
-        className={cn(
-          `object-cover w-full h-auto rounded-lg`,
-          onClick ? " hover:cursor-pointer" : ""
-        )}
-        onClick={onClick}
-      />
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div
+          className={cn(
+            "w-full h-full flex justify-center items-center mx-2 my-6",
+            className
+          )}
+        >
+          <Image
+            src={src}
+            alt={alt || "Image"}
+            width={500}
+            height={500}
+            className="w-full object-cover rounded-lg hover:cursor-pointer max-h-[440px]"
+          />
+        </div>
+      </DialogTrigger>
+      <DialogContent className="w-screen h-screen max-w-none rounded-none gap-0 pt-14 pb-0 px-0">
+        <div className="flex justify-center items-center w-full h-full">
+          <TransformWrapper minScale={0.75}>
+            <TransformComponent
+              wrapperStyle={{ height: "100%", width: "100%" }}
+            >
+              <Image
+                src={src}
+                width={2000}
+                height={2000}
+                alt={alt || "Zoomable"}
+                className="max-w-full max-h-[94vh] object-contain"
+                style={{ display: "block", margin: "0 auto" }}
+              />
+            </TransformComponent>
+          </TransformWrapper>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
