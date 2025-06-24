a823ad445a64b0eecf83d65bd3a8777747e3f2ae
NickVeles <xm1k123x@gmail.com>
2025-06-24 12:11:08 +0200

    fix: spread props in SectionContainer for better flexibility

--- components/utilities/section-container.tsx
+++ components/utilities/section-container.tsx
@@ -4,16 +4,29 @@ type SectionContainerProps = {
   accented?: boolean;
   className?: string;
   children?: React.ReactNode;
+  props: React.HTMLAttributes<HTMLDivElement>;
 };
 
 export default function SectionContainer({
   accented,
   className = "",
   children,
+  props,
 }: SectionContainerProps) {
   return (
-    <div className={`w-screen relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw] ${accented ? 'bg-accent' : ''}`}>
-      <div className={cn("sm:max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto w-full px-[1rem] py-[2rem] flex flex-col items-center justify-center", className)}>
+    <div
+      {...props}
+      className={cn(
+        "w-screen relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw]",
+        accented ? "bg-accent" : ""
+      )}
+    >
+      <div
+        className={cn(
+          "sm:max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto w-full px-[1rem] py-[2rem] flex flex-col items-center justify-center",
+          className
+        )}
+      >
         {children}
       </div>
     </div>
