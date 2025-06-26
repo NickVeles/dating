1584967aa07fe5174dc9ebb77d6e24d2dfd869d6
NickVeles <xm1k123x@gmail.com>
2025-06-26 20:42:00 +0200

    feat: move chips data to a separate constants file

---
+++ constants/chips.ts
@@ -0,0 +1,162 @@
+export const chips = [
+  {
+    name: "Core",
+    color: "bg-red-200 dark:bg-red-800 hover:bg-red-300 dark:hover:bg-red-700",
+    items: [
+      {
+        text: "6'3/190cm",
+        tooltip: "Height",
+        icon: "ruler",
+      },
+      {
+        text: "Polish",
+        tooltip: "Nationality",
+        icon: "flag",
+      },
+      {
+        text: "he/him",
+        tooltip: "Pronouns",
+        icon: "gender-male",
+      },
+      {
+        text: "Straight",
+        tooltip: "Orientation",
+        icon: "hetero",
+      },
+    ],
+  },
+  {
+    name: "Career",
+    color: "bg-sky-200 dark:bg-sky-800 hover:bg-sky-300 dark:hover:bg-sky-700",
+    items: [
+      {
+        text: "Software Dev",
+        tooltip: "Occupation",
+        icon: "suitcase",
+      },
+      {
+        text: "Web Dev",
+        tooltip: "Occupation",
+        icon: "suitcase",
+      },
+      {
+        text: "ML Specialist",
+        tooltip: "Occupation",
+        icon: "suitcase",
+      },
+      {
+        text: "IT Tech",
+        tooltip: "Education",
+        icon: "graduation-cap",
+      },
+      {
+        text: "Self-taught",
+        tooltip: "Education",
+        icon: "graduation-cap",
+      },
+    ],
+  },
+  {
+    name: "Values",
+    color: "bg-yellow-200 dark:bg-yellow-800 hover:bg-yellow-300 dark:hover:bg-yellow-700",
+    items: [
+      {
+        text: "Atheist/Agnostic",
+        tooltip: "Religion",
+        icon: "hands-praying",
+      },
+      {
+        text: "Monogamous",
+        tooltip: "Relationship style",
+        icon: "monogamy",
+      },
+      {
+        text: "Long-term",
+        tooltip: "Relationship expectations",
+        icon: "lock-heart",
+      },
+      {
+        text: "Single & Looking",
+        tooltip: "Relationship status",
+        icon: "magnifying-glass-heart",
+      },
+      {
+        text: "None | Open to having",
+        tooltip: "Children",
+        icon: "baby-carriage",
+      },
+    ],
+  },
+  {
+    name: "Lifestyle",
+    color: "bg-emerald-200 dark:bg-emerald-800 hover:bg-emerald-300 dark:hover:bg-emerald-700",
+    items: [
+      {
+        text: "5d/week",
+        tooltip: "Workout",
+        icon: "barbell",
+      },
+      {
+        text: "Low-fat | Hates pork",
+        tooltip: "Diet",
+        icon: "fork-knife",
+      },
+      {
+        text: "Never",
+        tooltip: "Smoking",
+        icon: "cigarette",
+      },
+      {
+        text: "Never",
+        tooltip: "Drinking",
+        icon: "wine",
+      },
+      {
+        text: "Never",
+        tooltip: "Substances",
+        icon: "pill",
+      },
+    ],
+  },
+  {
+    name: "Other",
+    color: "bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700",
+    items: [
+      {
+        text: "Dog",
+        tooltip: "Pet",
+        icon: "paw-print",
+      },
+      {
+        text: "Cat",
+        tooltip: "Pet",
+        icon: "paw-print",
+      },
+      {
+        text: "Turtle",
+        tooltip: "Pet",
+        icon: "paw-print",
+      },
+      {
+        text: "Stick-bugs",
+        tooltip: "Pet",
+        icon: "paw-print",
+      },
+      {
+        text: "English (C2)",
+        tooltip: "Language proficiency",
+        icon: "lang-english",
+      },
+      {
+        text: "Polish (C2)",
+        tooltip: "Language proficiency",
+        icon: "lang-polish",
+      },
+      {
+        text: "Mandarin (HSK1)",
+        tooltip: "Language proficiency",
+        icon: "lang-chinese",
+      },
+    ],
+  },
+];
