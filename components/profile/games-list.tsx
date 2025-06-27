5e17c5ba4c319462de5137b9c4110e807f0bce79
NickVeles <xm1k123x@gmail.com>
2025-06-27 11:45:44 +0200

    feat: add GamesList component with game images and tooltips

---
+++ components/profile/games-list.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Tooltip, TooltipTrigger, TooltipContent } from "../ui/tooltip";
+
+const games = ["roblox", "minecraft", "rivals"];
+
+export default function GamesList() {
+  return (
+    <div className="flex justify-center gap-1 m-1">
+      {games.map((game) => (
+        <Tooltip>
+          <TooltipTrigger>
+            <Image
+              src={`/images/${game}.png`}
+              width={24}
+              height={24}
+              alt={game}
+            />
+          </TooltipTrigger>
+          <TooltipContent>{game[0].toUpperCase() + game.slice(1)}</TooltipContent>
+        </Tooltip>
+      ))}
+    </div>
+  );
+}
