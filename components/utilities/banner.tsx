7e1b574f80dc53e6a5960fd64ece6c559bf9f653
NickVeles <xm1k123x@gmail.com>
2025-06-27 14:56:43 +0200

    style: change font from serif to sans in Banner component subtitle

--- components/utilities/banner.tsx
+++ components/utilities/banner.tsx
@@ -23,7 +23,7 @@ export default function Banner({ children, url, alt, icon, className }: BannerPr
       <div className="w-full flex flex-col justify-center items-center my-2 py-2 text-white bg-gray-800/80 shadow">
         <Image src={icon} width={32} height={32} alt={alt || "Banner icon"} className="invert" />
         {children && (
-          <H4 className="font-serif dyslexic:font-dyslexic text-center">
+          <H4 className="font-sans dyslexic:font-dyslexic text-center">
             {children}
           </H4>
         )}
