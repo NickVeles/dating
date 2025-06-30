9809dd5c16d012792dc15ed1911646836863b543
NickVeles <xm1k123x@gmail.com>
2025-06-30 17:28:39 +0200

    fix: update parameter type definition in PostPage component and change TypeScript ignore comment

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,8 +5,8 @@ import matter from "gray-matter";
 import { notFound } from "next/navigation";
 import TitleContainer from "@/components/utilities/title-container";
 
-export default async function PostPage({ params }: any) {
-  // @ts-ignore
+export default async function PostPage({ params }: { params: { slug: string }}) {
+  // @ts-expect-error
   const { slug } = await params;
   const postsDir = path.join(process.cwd(), "posts");
   const fullPath = path.join(postsDir, `${slug}.mdx`);
