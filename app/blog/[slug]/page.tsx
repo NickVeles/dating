4e7ebec0b41ac5e2680eafda5a5054f3b77cc644
NickVeles <xm1k123x@gmail.com>
2025-06-30 17:30:43 +0200

    fix: clarify TypeScript ignore comment in PostPage component

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { notFound } from "next/navigation";
 import TitleContainer from "@/components/utilities/title-container";
 
 export default async function PostPage({ params }: { params: { slug: string }}) {
-  // @ts-expect-error
+  // @ts-expect-error STOP EXPECTING THIS AS A PROMISE
   const { slug } = await params;
   const postsDir = path.join(process.cwd(), "posts");
   const fullPath = path.join(postsDir, `${slug}.mdx`);
