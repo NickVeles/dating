d1fa399b09f1c2ddc9e054ce978cea4b45344a20
NickVeles <xm1k123x@gmail.com>
2025-06-30 16:27:40 +0200

    feat: enhance BlogClient component with improved layout and additional post details

--- app/blog/blog-client.tsx
+++ app/blog/blog-client.tsx
@@ -1,32 +1,60 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { HeartStraight } from 'phosphor-react'
-import TitleContainer from '@/components/utilities/title-container'
+import Link from "next/link";
+import { HeartStraight } from "phosphor-react";
+import TitleContainer from "@/components/utilities/title-container";
+import SectionContainer from "@/components/utilities/section-container";
+import Image from "next/image";
+import { H3 } from "@/components/utilities/typography";
+
+const DefaultImage =
+  "https://images.unsplash.com/photo-1526047932273-341f2a7631f9?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 type Post = {
-  slug: string
-  title: string
-  date: string
-}
+  slug: string;
+  title: string;
+  date: string;
+  description?: string;
+  image?: string;
+};
 
 interface BlogClientProps {
-  posts: Post[]
+  posts: Post[];
 }
 
 export default function BlogClient({ posts }: BlogClientProps) {
   return (
     <main className="flex flex-col flex-1 gap-4">
       <TitleContainer Icon={HeartStraight}>Guide Blog</TitleContainer>
-      <ul>
-        {posts.map(({ slug, title, date }) => (
-          <li key={slug}>
-            <Link href={`/blog/${slug}`}>
-              {title} - {date}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <SectionContainer accented>
+        <ul className="flex flex-col w-full gap-4">
+          {posts.map(({ slug, title, date, description, image }) => (
+            <li key={slug} className="w-full">
+              <Link
+                href={`/blog/${slug}`}
+                className="flex flex-col lg:flex-row min-w-full p-2 gap-4 bg-background border border-black/20 dark:border-white/20 rounded-lg"
+              >
+                <Image
+                  src={image ?? DefaultImage}
+                  alt={title}
+                  width={800}
+                  height={480}
+                  className="flex flex-1 object-cover rounded aspect-[5/2] lg:aspect-[5/3] w-full"
+                />
+                <div className="flex flex-2 flex-col w-full px-2 lg:py-2 lg:px-0">
+                  <H3 className="font-serif dyslexic:font-dyslexic dyslexic:text-xl text-wrap">
+                    {title}
+                  </H3>
+                  <p className="font-sans dyslexic:font-dyslexic dyslexic:text-sm">
+                    {date}
+                  </p>
+                  <p className="pt-2 font-sans dyslexic:font-dyslexic text-sm dyslexic:text-xs">{description}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </SectionContainer>
     </main>
-  )
+  );
 }
