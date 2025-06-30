085fb01e01209e1f65daaaa439c9c9c94bf09bb9
NickVeles <xm1k123x@gmail.com>
2025-06-30 16:08:22 +0200

    feat: add 'images.unsplash.com' to allowed image domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withMDX = require('@next/mdx')({
 /** @type {import('next').NextConfig} */
 const nextConfig: import('next').NextConfig = withMDX({
   images: {
-    domains: ['i.scdn.co', 'i1.sndcdn.com'],
+    domains: ['i.scdn.co', 'i1.sndcdn.com', 'images.unsplash.com'],
   },
   pageExtensions: ['tsx', 'ts', 'jsx', 'js', 'mdx'],
 })
