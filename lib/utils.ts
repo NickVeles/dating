026afa471333e9a33baa8b539f7a10daf0d74e94
NickVeles <xm1k123x@gmail.com>
2025-07-06 21:52:58 +0200

    feat: add formatted date function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function articleDateTime(date: string | Date): string | null {
+  console.log()
+  const d = typeof date === "string" ? new Date(date) : date;
+
+  if (!(d instanceof Date) || isNaN(d.getTime())) {
+    return null;
+  }
+
+  return d.toLocaleString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
