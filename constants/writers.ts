695b101aa1bde238cc28c11adf9aeb47086dfbd3
NickVeles <xm1k123x@gmail.com>
2025-07-06 22:44:28 +0200

    feat: add constant list for writers

---
+++ constants/writers.ts
@@ -0,0 +1,13 @@
+interface Writer {
+  id: number,
+  github: string,
+  name: string,
+}
+
+export const writers: Writer[] = [
+  {
+    id: 1,
+    github: "NickVeles",
+    name: "Nick Veles"
+  },
+]
