123a8f9298382781141e46c8ccdc11bbd671aee0
NickVeles <xm1k123x@gmail.com>
2025-07-14 00:31:52 +0200

    fix: stop using radix

--- app/page-client.tsx
+++ app/page-client.tsx
@@ -11,8 +11,7 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { Accordion, AccordionContent, AccordionTrigger } from "@/components/ui/accordion";
-import { AccordionItem } from "@radix-ui/react-accordion";
+import { Accordion, AccordionContent, AccordionTrigger, AccordionItem } from "@/components/ui/accordion";
 
 const DefaultImage =
   "https://images.unsplash.com/photo-1526047932273-341f2a7631f9?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
