64a7461bf2b8572fd9084d6dbb2af12b153c26e3
NickVeles <xm1k123x@gmail.com>
2025-07-14 23:48:18 +0200

    fix: moved to tailwind units

--- components/utilities/title-container.tsx
+++ components/utilities/title-container.tsx
@@ -10,7 +10,7 @@ export default function TitleContainer({
   children,
 }: TitleContainerProps) {
   return (
-    <div className="flex flex-col gap-2 sm:max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto w-full p-[1rem] items-center justify-center">
+    <div className="flex flex-col gap-2 sm:max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto w-full p-4 items-center justify-center">
       {Icon && <Icon className="w-24 h-24 mx-auto text-primary" />}
       <h1 className="text-4xl text-center font-bold font-serif dyslexic:font-[family-name:var(--font-dyslexic)]">
         {children}
