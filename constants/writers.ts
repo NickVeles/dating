4922f8c57d42ec3bab9b19c91490c9c34c9846bc
NickVeles <xm1k123x@gmail.com>
2025-07-15 20:03:48 +0200

    feat: added anonymous writer

--- constants/writers.ts
+++ constants/writers.ts
@@ -1,13 +1,18 @@
 interface Writer {
   id: number,
-  github: string,
+  github?: string,
   name: string,
 }
 
 export const writers: Writer[] = [
   {
+    id: 0,
+    github: undefined,
+    name: "Anonymous",
+  },
+  {
     id: 1,
     github: "NickVeles",
-    name: "Nick Veles"
+    name: "Nick Veles",
   },
 ]
