0d3b7b99df0888561a42b955bccb7cbf50a34975
NickVeles <xm1k123x@gmail.com>
2025-07-16 17:10:05 +0200

    fix: disable ban-ts-comment - why is this even a thing?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,10 +12,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
-  
   {
     rules: {
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
     },
   },
 ];
